f163f49e0197f722651c33489a1c60a7893f4266
Ersheen <toersheen@gmail.com>
2025-07-11 01:20:49 +0530

    last time

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 
 app = Flask(__name__)
 app.secret_key = "your_secret_key"  # Replace with a strong secret key
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg://postgres:8xCE7MNbGnQL0FoF@db.jjadgttgbjpelumfqcsq.supabase.co:5432/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 CORS(app)  
