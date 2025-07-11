51c8497a5309ba07c780a2eb3e3e5252f80399f0
Ersheen <81225733+Ersheen@users.noreply.github.com>
2025-07-11 10:28:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_cors import CORS  # Optional, if doing cross-origin requests
 from datetime import datetime, timedelta
-# import psycopg
 
 app = Flask(__name__)
 app.secret_key = "your_secret_key"  # Replace with a strong secret key
